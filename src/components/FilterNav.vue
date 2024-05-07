9bddc134cbb24b275d7849e8160806535aa6f8c0
esrafil elahi <esrafil.elahi@gmail.com>
2024-05-07 13:41:27 +0330

    add filter nav

--- src/components/FilterNav.vue
+++ src/components/FilterNav.vue
@@ -1,28 +1,33 @@
 <template>
   <div class="flex-center gap-4">
-    <button class="btn" @click="handleGetAllPhotos">All Album</button>
-    <button class="btn" @click="handleGetPhotoByAlbumId(1)">Album 1</button>
-    <button class="btn" @click="handleGetPhotoByAlbumId(2)">Album 2</button>
-    <button class="btn" @click="handleGetPhotoByAlbumId(3)">Album 3</button>
+    <button class="btn" :class="{ 'text-white': selectedFilterNav === 'all' }" @click="handleGetAllPhotos">All
+      Album</button>
+    <button class="btn" :class="{ 'text-white': selectedFilterNav === 'album_1' }"
+      @click="handleGetPhotoByAlbumId(1)">Album 1</button>
+    <button class="btn" :class="{ 'text-white': selectedFilterNav === 'album_2' }"
+      @click="handleGetPhotoByAlbumId(2)">Album 2</button>
+    <button class="btn" :class="{ 'text-white': selectedFilterNav === 'album_3' }"
+      @click="handleGetPhotoByAlbumId(3)">Album 3</button>
   </div>
 </template>
 
 <script>
-// import { mapState } from 'vuex';
-
 export default {
   name: "FilterNav",
+
+  props: ['selectedFilterNav', 'setSelectedFilterNav'],
 
   methods: {
     handleGetAllPhotos() {
       this.$store.dispatch('photos/getAllPhotos')
+      this.setSelectedFilterNav('all')
     },
 
     handleGetPhotoByAlbumId(albumId) {
       this.$store.dispatch('photos/getAlbumPhotosByAlbumId', albumId)
+      this.setSelectedFilterNav(`album_${albumId}`)
     }
   }
 
-  // computed: mapState({})
 }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="overflow-x-hidden">
-    <FilterNav />
+    <FilterNav :selectedFilterNav="selectedFilterNav" :setSelectedFilterNav="setSelectedFilterNav" />
 
     <div class="flex-between flex-wrap overflow-x-hidden mt-10 gap-5">
-      <div v-for="photo in photos" :key="photo.id" class="flex flex-col flex-center w-[25rem]">
+      <div v-for="photo in filterPhotos" :key="photo.id" class="flex flex-col flex-center w-[25rem]">
         <PhotoItemVue :photo="photo" />
       </div>
     </div>
@@ -19,17 +19,37 @@ import PhotoItemVue from '@/components/PhotoItem.vue';
 export default {
   name: '-Home-',
   data() {
-    return {}
+    return {
+      selectedFilterNav: 'all'
+    }
   },
   components: { FilterNav, PhotoItemVue },
+
+  methods: {
+    setSelectedFilterNav(nav) {
+      this.selectedFilterNav = nav
+    }
+  },
 
   computed: {
     ...mapState({
       photos: (state) => state.photos.allPhotos,
       basket: (state) => state.photos.basket,
+      album1: (state) => state.photos.album1,
+      album2: (state) => state.photos.album2,
+      album3: (state) => state.photos.album3,
     }),
+    // eslint-disable-next-line vue/return-in-computed-property
     filterPhotos() {
-      return this.photos
+      if (this.selectedFilterNav === 'album_1') {
+        return this.album1
+      } else if (this.selectedFilterNav === 'album_2') {
+        return this.album2
+      } else if (this.selectedFilterNav === 'album_3') {
+        return this.album3
+      } else {
+        return this.photos
+      }
     }
   },
 
